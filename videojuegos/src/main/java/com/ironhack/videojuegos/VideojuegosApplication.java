d902d36d9db41c4865364c9e1903c291f147d56d
Fregaladol <friigaiden@gmail.com>
2025-07-21 22:07:13 +0200

    Add files via upload

---
+++ videojuegos/src/main/java/com/ironhack/videojuegos/VideojuegosApplication.java
@@ -0,0 +1,17 @@
+package com.ironhack.videojuegos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideojuegosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideojuegosApplication.class, args);
+
+		GestorVG2 pruebaJDBC = new GestorVG2();
+		pruebaJDBC.consulta();
+	
+	}
+
+}
